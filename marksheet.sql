ed31c49485955b9180f140d56ec4e2c067b87549
HetDholariya3 <k.radhe3847@gmail.com>
2025-06-10 17:30:25 +0530

    Add files via upload

---
+++ marksheet.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 10, 2025 at 01:41 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `marksheet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `results`
+--
+
+CREATE TABLE `results` (
+  `fname` varchar(50) DEFAULT NULL,
+  `lname` varchar(50) DEFAULT NULL,
+  `enrollment` int(20) DEFAULT NULL,
+  `division` varchar(10) DEFAULT NULL,
+  `semester` int(10) DEFAULT NULL,
+  `ds` double DEFAULT NULL,
+  `laravel` double DEFAULT NULL,
+  `c_language` double DEFAULT NULL,
+  `java` double DEFAULT NULL,
+  `python` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `results`
+--
+
+INSERT INTO `results` (`fname`, `lname`, `enrollment`, `division`, `semester`, `ds`, `laravel`, `c_language`, `java`, `python`) VALUES
+('het', 'd', 2147483647, 'a', 3, 20, 60, 66, 77, 88),
+('Umag', 'm', 2147483647, 'B', 5, 33, 55, 77, 99, 20),
+('a', 'b', 2147483647, 'F', 4, 66, 55, 44, 77, 44),
+('amit', 's', 2147483647, 'C', 3, 22, 33, 44, 55, 66),
+('m', 'm', 2147483647, 'A', 5, 44, 77, 55, 66, 99);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
